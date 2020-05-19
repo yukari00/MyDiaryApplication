ef3c62742dfe24d3fbf0434d9696eb202a22e111
yukari00 <62502431+yukari00@users.noreply.github.com>
2020-05-19 14:56:29 +0900

    google login

--- app/src/main/java/com/example/mydiaryapplication/ListActivity.kt
+++ app/src/main/java/com/example/mydiaryapplication/ListActivity.kt
@@ -1,12 +1,35 @@
 package com.example.mydiaryapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        sign_out_button.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        startActivity(MainActivity.getLaunchIntent(this))
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, ListActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
     }
 }
