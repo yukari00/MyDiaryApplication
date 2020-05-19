7937eca63c5dde3e2e896df575324c7482e3bf18
yukari00 <62502431+yukari00@users.noreply.github.com>
2020-05-19 20:02:35 +0900

    #2

--- app/src/main/java/com/example/mydiaryapplication/MainActivity.kt
+++ app/src/main/java/com/example/mydiaryapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -22,9 +23,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var mGoogleSignInOptions: GoogleSignInOptions
     private lateinit var firebaseAuth: FirebaseAuth
 
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            startActivity(ListActivity.getLaunchIntent(this))
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         configureGoogleSignIn()
         setupUI()
@@ -57,7 +69,8 @@ class MainActivity : AppCompatActivity() {
                 val account = task.getResult(ApiException::class.java)
                 firebaseAuthWithGoogle(account!!)
             } catch (e: ApiException) {
-                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+                Log.d("errorrrr","ApiExceptionは${e}です")
+                Toast.makeText(this, "Google sign in failed1:(", Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -69,19 +82,11 @@ class MainActivity : AppCompatActivity() {
 
                 startActivity(ListActivity.getLaunchIntent(this))
             } else {
-                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Google sign in failed2:(", Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val user = FirebaseAuth.getInstance().currentUser
-        if (user != null) {
-            startActivity(ListActivity.getLaunchIntent(this))
-            finish()
-        }
-    }
 
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, MainActivity::class.java).apply {
