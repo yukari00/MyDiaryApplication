06ea3dd1084941a46e8803e350ad6c1868260704
yukari00 <62502431+yukari00@users.noreply.github.com>
2020-05-19 23:06:09 +0900

    save new data

--- app/src/main/java/com/example/mydiaryapplication/EditActivity.kt
+++ app/src/main/java/com/example/mydiaryapplication/EditActivity.kt
@@ -2,6 +2,10 @@ package com.example.mydiaryapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
@@ -10,7 +14,38 @@ class EditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit)
 
         val bundle = intent.extras
-        
+
+        //ボタンはToolbar作成後、修正
+        button_edit_done.setOnClickListener {
+            addNewData()
+        }
+
+    }
+
+    private fun addNewData() {
+
+        val database = FirebaseFirestore.getInstance()
+        //本当はcallendarクラスから日付を取得するが、今は一旦適当な日付を
+        val date = "2020年5月19日"
+        val setDetail = input_edit_detail.text.toString()
+
+        if(setDetail == ""){
+            inputDetail.error = "入力してください"
+            return
+        }
+
+        val newData = hashMapOf<String, String>("date" to date, "detail" to setDetail)
+        database.collection("collection").document(date).set(newData)
+            .addOnSuccessListener {
+                Log.d("TAG", "DocumentSnapshot successfully written!")
+                Toast.makeText(this, "登録が完了しました", Toast.LENGTH_LONG).show()
+                finish()
+
+            }
+            .addOnFailureListener {
+                    e -> Log.w("TAG", "Error writing document", e)
+                Toast.makeText(this, "登録ができませんでした", Toast.LENGTH_LONG).show()
+            }
     }
 
 }
