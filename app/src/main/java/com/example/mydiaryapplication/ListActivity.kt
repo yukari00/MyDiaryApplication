93c15bf14b3b1ccd53517197cf1a68a5ace106c5
yukari00 <62502431+yukari00@users.noreply.github.com>
2020-05-20 15:21:43 +0900

    List画面で保存したテキストの一覧データをFirestoreから取得し、ログ出力で確認

--- app/src/main/java/com/example/mydiaryapplication/ListActivity.kt
+++ app/src/main/java/com/example/mydiaryapplication/ListActivity.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
@@ -16,11 +18,26 @@ class ListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list)
 
         setupUI()
-
+        showList()
         floating_button.setOnClickListener {
 
             startActivity(EditActivity.getLaunchIntent(this))
         }
+    }
+
+    private fun showList() {
+        val database = FirebaseFirestore.getInstance()
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+        database.collection("users").document(userId).collection("notes")
+            .get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d("CHECK THIS", "${document.id} => ${document.data}")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d("CHECK THIS", "Error getting documents: ", exception)
+            }
     }
 
     private fun setupUI() {
